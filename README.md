1d4bd31b6767e448ad02cd8c2676dc84b6cf3ba2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-07-29 09:49:07 +0200

    Publish of Github pages from Gradle.

--- README.md
+++
@@ -1,1 +0,0 @@
-Content of the Ontrack web pages.
--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-var html = "";
-$.each(releases, function (index, release) {
-   html += "<tr>"
-      + '<td><a href="https://github.com/nemerosa/ontrack/releases/tag/' + release + '">' + release + '</a></td>'
-      + '<td><a href="release/' + release + '/doc/index.html">HTML</a> | <a href="release/' + release + '/index.pdf">PDF</a> | <a href="release/' + release + '/javadoc/index.html">Javadoc</a></td>'
-      + "</tr>";
-});
-$('#ontrack-doc-body').html(html);
