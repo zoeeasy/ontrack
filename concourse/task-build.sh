4a3580833d443fb386c374ed1296ec564e1b9879
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-10-05 17:51:16 +0200

    [concourse] Gradle build

--- concourse/task-build.sh
+++ concourse/task-build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hey"
+./gradlew \
+    clean \
+    versionDisplay \
+    versionFile \
+    test \
+    build \
+    -PbowerOptions='--allow-root' \
+    -Dorg.gradle.jvmargs=-Xmx1536m \
+    --info \
+    --stacktrace \
+    --profile \
+    --console plain
