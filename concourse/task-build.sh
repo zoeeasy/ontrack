ab7d714a9a33a5807111a470a67a0fd043caac3f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-10-05 17:59:05 +0200

    [concourse] Using input = core and correct shell script settings

--- concourse/task-build.sh
+++ concourse/task-build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-./gradlew \
-    clean \
-    versionDisplay \
-    versionFile \
-    test \
-    build \
-    -PbowerOptions='--allow-root' \
-    -Dorg.gradle.jvmargs=-Xmx1536m \
-    --info \
-    --stacktrace \
-    --profile \
-    --console plain
+set -e -x
+
+pushd code
+    ./gradlew \
+        clean \
+        versionDisplay \
+        versionFile \
+        test \
+        build \
+        -PbowerOptions='--allow-root' \
+        -Dorg.gradle.jvmargs=-Xmx1536m \
+        --info \
+        --stacktrace \
+        --profile \
+        --console plain
+popd
