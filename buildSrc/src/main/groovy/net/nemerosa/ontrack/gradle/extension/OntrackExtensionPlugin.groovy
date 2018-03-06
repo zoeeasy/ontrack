981139884d2c721c1535f42afeb1bc68d5b6d2a6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-03-06 19:37:08 +0100

    [pipeline] Upgrade of Gradle Node plugin

--- buildSrc/src/main/groovy/net/nemerosa/ontrack/gradle/extension/OntrackExtensionPlugin.groovy
+++ buildSrc/src/main/groovy/net/nemerosa/ontrack/gradle/extension/OntrackExtensionPlugin.groovy
@@ -1,7 +1,6 @@
 package net.nemerosa.ontrack.gradle.extension
 
 import com.moowork.gradle.node.task.NodeTask
-import com.moowork.gradle.node.task.NpmTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -21,73 +20,59 @@ class OntrackExtensionPlugin implements Plugin<Project> {
          * NPM setup
          */
 
+        project.ext {
+            nodeDir = project.file("${project.buildDir}")
+        }
+
         project.apply plugin: 'com.moowork.node'
         project.node {
+
             version = '4.2.2'
-            npmVersion = '4.2.2'
+            npmVersion = '2.14.7'
             download = true
-            workDir = project.file("${project.projectDir}/.gradle/nodejs")
+
+            // Set the work directory for unpacking node
+            workDir = project.file("${project.buildDir}/nodejs")
+
+            // Set the work directory for NPM
+            npmWorkDir = project.file("${project.buildDir}/npm")
+
+            // Set the work directory where node_modules should be located
+            nodeModulesDir = project.nodeDir
         }
 
         /**
          * NPM tasks
          */
 
-        project.ext {
-            cacheDir = project.gradle.getGradleUserHomeDir() as String ?: "${System.getProperty("user.home")}/.cache/gradle"
-            npmCacheDir = "${cacheDir}/caches/npm"
-            ontrackCacheDir = "${cacheDir}/caches/ontrack/extension"
-        }
-
-        project.tasks.create('npmCacheConfig', NpmTask) {
-            doFirst {
-                println "[ontrack] Configure the NPM cache in ${project.npmCacheDir}"
-            }
-            args = [ 'config', 'set', 'cache', project.npmCacheDir ]
-            outputs.dir project.file(project.npmCacheDir)
-        }
-
         project.tasks.create('copyPackageJson') {
             // Without any input reference, the target file will never be replaced
-            // outputs.file new File(project.ontrackCacheDir as String, 'package.json')
+            // outputs.file new File(project.nodeDir as String, 'package.json')
             doLast {
-                println "[ontrack] Copies the package.json file to ${project.ontrackCacheDir}"
-                project.mkdir new File(project.ontrackCacheDir as String)
-                new File(project.ontrackCacheDir as String, 'package.json').text = getClass().getResourceAsStream('/extension/package.json').text
+                println "[ontrack] Copies the package.json file to ${project.nodeDir}"
+                project.mkdir new File(project.nodeDir as String)
+                new File(project.nodeDir as String, 'package.json').text = getClass().getResourceAsStream('/extension/package.json').text
             }
         }
 
         project.tasks.create('copyGulpFile') {
             // Without any input reference, the target file will never be replaced
-            // outputs.file new File(project.ontrackCacheDir as String, 'gulpfile.js')
+            // outputs.file new File(project.nodeDir as String, 'gulpfile.js')
             doLast {
-                println "[ontrack] Copies the gulpfile.js file to ${project.ontrackCacheDir}"
-                project.mkdir new File(project.ontrackCacheDir as String)
-                new File(project.ontrackCacheDir as String, 'gulpfile.js').text = getClass().getResourceAsStream('/extension/gulpfile.js').text
+                println "[ontrack] Copies the gulpfile.js file to ${project.nodeDir}"
+                project.mkdir new File(project.nodeDir as String)
+                new File(project.nodeDir as String, 'gulpfile.js').text = getClass().getResourceAsStream('/extension/gulpfile.js').text
             }
         }
 
-        project.tasks.create('npmPackages', NpmTask) {
-            def nodeModulesDir = new File(project.ontrackCacheDir as String, 'node_modules')
-
-            dependsOn project.tasks.npmCacheConfig
-            dependsOn project.tasks.copyPackageJson
-
-            inputs.file new File(project.ontrackCacheDir as String, 'package.json')
-            outputs.dir nodeModulesDir
-
-            doFirst { println "[ontrack] Install Node.js packages in ${nodeModulesDir}" }
-
-            workingDir = new File(project.ontrackCacheDir as String)
-            args = [ 'install' ]
-        }
+        project.tasks.npmInstall.dependsOn('copyPackageJson')
 
         /**
          * Gulp call
          */
 
         project.tasks.create('web', NodeTask) {
-            dependsOn 'npmPackages'
+            dependsOn 'npmInstall'
             dependsOn 'copyGulpFile'
 
             inputs.dir project.file('src/main/resources/static')
@@ -98,8 +83,11 @@ class OntrackExtensionPlugin implements Plugin<Project> {
                 println "[ontrack] Generating web resources of ${project.extensions.ontrack.id(project)} in ${project.buildDir}"
             }
 
-            workingDir = new File(project.ontrackCacheDir as String)
-            script = new File(new File(project.ontrackCacheDir as String, 'node_modules'), 'gulp/bin/gulp')
+            execOverrides {
+                it.workingDir = new File(project.nodeDir as String)
+            }
+
+            script = new File(new File(project.nodeDir, 'node_modules'), 'gulp/bin/gulp')
             args = [
                     'default',
                     '--extension', project.extensions.ontrack.id(project),
@@ -141,35 +129,6 @@ version = ${project.version}
             exclude 'static/**/*.js'
             exclude 'static/**/*.html'
         }
-
-        /**
-         * Custom configuration to package a module's own dependencies, minus the ones
-         * provided by Ontrack itself.
-         */
-
-//        project.configurations {
-//            moduleDependencies {
-//                extendsFrom project.configurations.runtime
-//                exclude group: 'net.nemerosa.ontrack'
-//                exclude group: 'org.springframework'
-//                exclude group: 'org.apache.httpcomponents'
-//                exclude group: 'org.codehaus.groovy'
-//                exclude group: 'org.projectlombok'
-//                exclude group: 'org.slf4j'
-//                exclude group: 'commons-io'
-//                exclude group: 'com.google.guava'
-//            }
-//        }
-
-        /**
-         * Spring Boot packaging as a module
-         */
-
-//        project.apply plugin: 'spring-boot'
-//        project.springBoot {
-//            layout = 'MODULE'
-//            customConfiguration = 'moduleDependencies'
-//        }
 
     }
 
